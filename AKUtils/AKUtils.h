b536183df1d466723549587c7a530d0d2fc6d534
Alper KIRDOK <alperkirdok@gmail.com>
2015-11-25 21:39:57 +0200

    Initial Commit

---
+++ AKUtils/AKUtils.h
@@ -0,0 +1,19 @@
+//
+//  AKUtils.h
+//  AKUtils
+//
+//  Created by Alper KIRDÖK on 25/11/15.
+//  Copyright © 2015 Alper KIRDÖK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AKUtils.
+FOUNDATION_EXPORT double AKUtilsVersionNumber;
+
+//! Project version string for AKUtils.
+FOUNDATION_EXPORT const unsigned char AKUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKUtils/PublicHeader.h>
+
+
